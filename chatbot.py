7fdc6d92b40ebe288d23b30456c827b8a814a895
Vartikamishra07 <113111976+Vartikamishra07@users.noreply.github.com>
2024-05-11 02:50:35 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,30 @@
 search_tree = {
-    "Admission": {
+    "Admission": 
+    {
         "Cutoff": "The cutoff is > 99 percentile for MHTCET. For more info, refer https://pict.edu/cutoff-FE/",
         "Fee": "The fees is 93K for open-category candidates. For more info, refer https://pict.edu/fee-structure/",
         "Branch": "Branches are COMP, IT, EnTC, ECE, AIDS",
     },
 
     "Hostel": "The campus has a girls and boys hostel in-premises. For more info, refer https://pict.edu/hostel/",
-    "Placement": {
+   
+    "Placement": 
+    {
         "Statistic": "For this year, 706 students are placed. For detailed statistics, refer https://pict.edu/placement/index.php#statistics",
         "Training": "https://pict.edu/placement/index.php#trainingactivities",
     },
 
     "Library": "PICT central library being information hub provides access to full text, digital and printed resources to support the scholarly and informational needs of the students, faculty, researchers, and other users.",
+    
     "Timetable": "https://pict.edu/time_table_syllabus/",
+    
     "Syllabus": "https://pict.edu/time_table_syllabus/",
 }
 
 def search(query, subtree):
     query = query.lower()  # Convert query to lowercase
     if type(subtree) == str:
-        print(f"Reply: {subtree}")
+        print("Reply\n" , subtree)
         print("-------------")
         return
     
